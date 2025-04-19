1935c6629872e1d9b69cd36afdaa3302f4edcec8
Doudousmyle42 <falldoudou4234@gmail.com>
2025-04-19 11:39:21 +0100

    Update medecin.cpp

--- medecin.cpp
+++ medecin.cpp
@@ -9,19 +9,7 @@ using namespace std;
     this->specialite = specialite;
     this->salaire = salaire;
 }
- /*Medecin::Medecin(string nom, string prenom, int age, string specialite, int salaire) {
-    this->nom = nom;
-    this->prenom = prenom;
 
-    if (age <= 0) {
-        cout << "l'age doit etre superieur a 0" << endl;
-    } else {
-        this->age = age;
-    }
-
-    this->specialite = specialite;
-    this->salaire = salaire;
-}*/
  //setters
  void Medecin::setspecialite(string specialite){
     this -> specialite = specialite;
